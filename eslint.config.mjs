5fa46bf16c4f539989cd33a5966ec717c8ec983b
Mateus Borba <santosmatesu2001@gmail.com>
2025-02-19 00:49:32 -0300

    add: project setup and home page layout view

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,14 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "next/typescript",
+    "standard",
+    "plugin:tailwindcss/recommned",
+    "prettier"
+  ),
 ];
 
 export default eslintConfig;
